0ad4e4a10b1719ac39fcc3f5e6204c0947e056b3
Gyeltshen21 <12190049.gcit@rub.edu.bt>
2021-05-10 21:44:42 +0600

    TODO_15

---
+++ LabWork/TODO_15i/app/src/main/java/com/gcit/todo_15i/PagerAdapter.java
@@ -0,0 +1,36 @@
+package com.gcit.todo_15i;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        mNumOfTabs = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new TabFragment1();
+            case 1:
+                return new TabFragment2();
+            case 2:
+                return new TabFragment3();
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
