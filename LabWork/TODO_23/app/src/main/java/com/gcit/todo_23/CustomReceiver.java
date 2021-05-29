09af6ca702a309fbc54354bf322aae5fcac09873
Gyeltshen21 <12190049.gcit@rub.edu.bt>
2021-05-29 21:31:40 +0600

    TODO_23

---
+++ LabWork/TODO_23/app/src/main/java/com/gcit/todo_23/CustomReceiver.java
@@ -0,0 +1,33 @@
+package com.gcit.todo_23;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID+ "ACTION_CUSTOM_BROADCAST";
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if(intentAction !=null){
+            String toastMessage = "";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected!";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected";
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = "Custom Broadcast Recieved";
+                    break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
